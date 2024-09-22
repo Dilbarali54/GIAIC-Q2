bb3ffa3c51c205f1eff76c4a539e3913fe14bd52
Dilbar Ali <160045634+Dilbarali54@users.noreply.github.com>
2024-09-23 00:16:38 +0500

    Add files via upload

---
+++ 2nd-assignment/app/header.tsx
@@ -0,0 +1,15 @@
+
+export default function Header() {
+    return (
+      <header>
+      <h1> Wellcome To Next.Js</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+      </header>
+    );
+  }
+  
---
+++ 2nd-assignment/app/page.tsx
@@ -0,0 +1,12 @@
+
+import Header from "./header";
+import About from "./Components/about";
+
+export default function Home() {
+  return (
+ <div>
+  <Header/>
+  <About/>
+ </div>
+  );
+}
